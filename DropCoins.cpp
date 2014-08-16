ff2f380db9692e4fc4ba6df03fda7d6507cf6ee1
keshavnandan <nandan.keshav55@gmail.com>
2014-08-17 00:47:19 +0530

    srm 525 div 1 300

---
+++ DropCoins.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <cstring>
+#include <queue>
+using namespace std;
+
+class DropCoins {
+        public:
+
+        int getMinimum(vector <string> b, int k)
+        {
+            int m = b.size();
+            int n = b[0].size();
+            vector<int> row, col, rr, cc;
+            row.resize(m); rr.resize(m);
+            col.resize(n); cc.resize(n);
+            fill(row.begin(), row.end(), 0);
+            fill(col.begin(), col.end(), 0);
+            for(int i = 0; i < m; i++)
+            for(int j = 0; j < n; j++){
+                if(b[i][j] == 'o'){
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+            int ret = 1000000;
+            for(int l = 0; l <= n; l++)
+            for(int r = 0; l+r<=n; r++)
+            for(int u = 0; u <= m; u++)
+            for(int v = 0; u+v<=m; v++){
+                int cnt = 0;
+                vector<string> s = b;
+                for(int i = 0; i < m; i++)
+                for(int j = 0; j < n; j++)
+                if((i < u || i >= m-v) || (j < l || j >= n-r)) s[i][j] = '.';
+
+                for(int i = 0; i < m; i++)
+                for(int j = 0; j < n; j++)
+                    if(s[i][j] == 'o') cnt++;
+
+                if(cnt == k){
+                    ret = min(ret, l+r+min(l, r)+u+v+min(u, v));
+                }
+            }
+            if(ret == 1000000) return -1;
+            return ret;
+        }
+
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { string Arr0[] = {".o.."
+,"oooo"
+,"..o."}
+; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 3; int Arg2 = 2; verify_case(0, Arg2, getMinimum(Arg0, Arg1)); }
+	void test_case_1() { string Arr0[] = {".....o"
+,"......"
+,"oooooo"
+,"oooooo"
+,"......"
+,"o....."}
+; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 12; int Arg2 = 3; verify_case(1, Arg2, getMinimum(Arg0, Arg1)); }
+	void test_case_2() { string Arr0[] = {"...."
+,".oo."
+,".oo."
+,"...."}
+; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 3; int Arg2 = -1; verify_case(2, Arg2, getMinimum(Arg0, Arg1)); }
+	void test_case_3() { string Arr0[] = {"......."
+,"..ooo.."
+,"ooooooo"
+,".oo.oo."
+,"oo...oo"}
+; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 12; int Arg2 = 4; verify_case(3, Arg2, getMinimum(Arg0, Arg1)); }
+	void test_case_4() { string Arr0[] = {"................."
+,".ooooooo...oooo.."
+,".ooooooo..oooooo."
+,".oo.......oo..oo."
+,".oo.......oo..oo."
+,".ooooo.....oooo.."
+,".ooooooo...oooo.."
+,".....ooo..oo..oo."
+,"......oo..oo..oo."
+,".ooooooo..oooooo."
+,".oooooo....oooo.."
+,"................."}
+; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 58; int Arg2 = 6; verify_case(4, Arg2, getMinimum(Arg0, Arg1)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE
+     int main()
+     {
+        DropCoins ___test;
+        ___test.run_test(-1);
+     }
+// END CUT HERE
