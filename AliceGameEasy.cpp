56f4f3a4e4719ebc5b13588091bf045142f54fe9
keshavnandan <nandan.keshav55@gmail.com>
2014-11-28 20:09:57 +0530

    srm 639 div 2 500

---
+++ AliceGameEasy.cpp
@@ -0,0 +1,57 @@
+// BEGIN CUT HERE
+
+// END CUT HERE
+#include <iostream>
+#include <sstream>
+#include <set>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+class AliceGameEasy{ 
+        public: 
+        long long findMinimumValue(long long x, long long y) { 
+			long s = (x+y);
+			if (s == 0) return 0;
+			long k = sqrt(1+8*s);
+			if(k*k == (1+8*s)){
+
+				long n = (k-1)/2;
+				long sum = 0, c = 0, i = n;
+
+				while(i >= 0){
+					if(sum >= x) return c;
+					c++;
+					sum += i;
+					i--;
+				}
+			}
+			return -1;	           
+        } 
+        
+// BEGIN CUT HERE
+	public:
+	void run_test(long Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); if ((Case == -1) || (Case == 5)) test_case_5(); }
+	private:
+	template <typename T> string prlong_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(long Case, const long long &Expected, const long long &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { long long Arg0 = 7LL; long long Arg1 = 14LL; long long Arg2 = 2LL; verify_case(0, Arg2, findMinimumValue(Arg0, Arg1)); }
+	void test_case_1() { long long Arg0 = 10LL; long long Arg1 = 0LL; long long Arg2 = 4LL; verify_case(1, Arg2, findMinimumValue(Arg0, Arg1)); }
+	void test_case_2() { long long Arg0 = 932599670050LL; long long Arg1 = 67400241741LL; long long Arg2 = 1047062LL; verify_case(2, Arg2, findMinimumValue(Arg0, Arg1)); }
+	void test_case_3() { long long Arg0 = 7LL; long long Arg1 = 13LL; long long Arg2 = -1LL; verify_case(3, Arg2, findMinimumValue(Arg0, Arg1)); }
+	void test_case_4() { long long Arg0 = 0LL; long long Arg1 = 0LL; long long Arg2 = 0LL; verify_case(4, Arg2, findMinimumValue(Arg0, Arg1)); }
+	void test_case_5() { long long Arg0 = 100000LL; long long Arg1 = 400500LL; long long Arg2 = 106LL; verify_case(5, Arg2, findMinimumValue(Arg0, Arg1)); }
+
+// END CUT HERE
+ 
+}; 
+
+// BEGIN CUT HERE 
+int main(){
+
+        AliceGameEasy ___test; 
+        ___test.run_test(-1); 
+} 
+// END CUT HERE     
