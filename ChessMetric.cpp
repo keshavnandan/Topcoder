6dfb5fe73ffbea2e920c650d058f511e6914c8f2
keshavnandan <nandan.keshav55@gmail.com>
2015-02-01 16:55:15 +0530

    tccc 03 round 4 250 sol

--- ChessMetric.cpp
+++ ChessMetric.cpp
@@ -4,55 +4,41 @@
 #include <algorithm>
 #include <set>
 #include <map>
+#include <queue>
 #include <cstring>
+#include <climits>
+#include <cstdio>
 using namespace std;
-long long A[100][100];
-long long B[100][100];
+typedef pair<int,int> pi;
+typedef set<int> si;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+typedef long long ll;
+ll dp[105][105][55];
+int X[16] = {0, 0, 1, -1, 1, -1, -1, 1, 1, -1, 1, -1, 2, -2, 2, -2};
+int Y[16] = {1, -1, 0, 0, -1, 1, -1, 1, 2, 2, -2, -2, 1, 1, -1, -1};
 
-class ChessMetric {
-        public:
-        long long howMany(int n, vector <int> start, vector <int> end, int numMoves)
-        {
-            int s[16][2] = {1,1,-1,1,1,-1,-1,-1,1,2,1,-2,-1,2,-1,-2,2,1,2,-1,-2,1,-2,-1,0,1,0,-1,1,0,-1,0};
-
-            memset(A, 0, sizeof(A));
-            memset(B, 0, sizeof(B));
-            int i = end[0], j = end[1];
-            for(int k = 0; k < 16; k++)
-            {
-                int x = i+s[k][0], y = j+s[k][1];
-                if(x < n && y < n && x >= 0 && y >= 0){
-                    A[x][y] = 1ll;
-//                                cout<<x<<" "<<y<<endl;
+class ChessMetric{ 
+        public: 
+        long long howMany(int size, vector <int> start, vector <int> end, int numMoves) { 
+           for(int i = 0; i < 105; i++)
+            for(int j = 0; j < 105; j++)
+                for(int k = 0; k < 55; k++)
+                    dp[i][j][k] = 0;
+           dp[start[0]][start[1]][0] = 1;
+           for(int k = 0; k <= numMoves; k++)
+           for(int x = 0; x < size; x++)
+            for(int y = 0; y < size; y++){
+                for(int i = 0; i < 16; i++){
+                    int xx = x + X[i], yy = y + Y[i];
+                    if(xx >= 0 && yy >= 0 && xx <= size && yy <= size)
+                    dp[xx][yy][k+1] += dp[x][y][k];
                 }
             }
-
-
-            if(numMoves == 1) return A[start[0]][start[1]];
-
-            for(int ss = 2; ss <= numMoves; ss++){
-                if(ss%2) memset(A, 0, sizeof(A));
-                 else    memset(B, 0, sizeof(B));
-
-                for(int i = 0; i < n; i++)
-                    for(int j = 0; j < n; j++)
-                        for(int k = 0; k < 16; k++)
-                        {
-                            int x = i+s[k][0], y = j+s[k][1];
-                            if(x < n && y < n && x >= 0 && y >= 0){
-//                                cout<<x<<" "<<y<<endl;
-                                if(ss%2 && B[x][y]) A[i][j] += B[x][y];
-                                else if(!(ss%2) && A[x][y]) B[i][j] += A[x][y];
-                            }
-                        }
-            }
-
-            if(numMoves%2)
-                return A[start[0]][start[1]];
-            return B[start[0]][start[1]];
-
+           return dp[end[0]][end[1]][numMoves];
         }
-
+        
 // BEGIN CUT HERE
 	public:
 	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); }
@@ -66,13 +52,13 @@ class ChessMetric {
 	void test_case_4() { int Arg0 = 100; int Arr1[] = {0,0}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arr2[] = {0,99}; vector <int> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); int Arg3 = 50; long long Arg4 = 243097320072600LL; verify_case(4, Arg4, howMany(Arg0, Arg1, Arg2, Arg3)); }
 
 // END CUT HERE
+ 
+}; 
 
-};
+// BEGIN CUT HERE 
+int main(){
 
-// BEGIN CUT HERE
-     int main()
-     {
-        ChessMetric ___test;
-        ___test.run_test(-1);
-     }
-// END CUT HERE
+        ChessMetric ___test; 
+        ___test.run_test(-1); 
+} 
+// END CUT HERE     
