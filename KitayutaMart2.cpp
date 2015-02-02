4d09120060eeb3b08460a8a9eaa8e815f2247433
keshavnandan <nandan.keshav55@gmail.com>
2015-02-02 19:42:51 +0530

    srm 648 div 2 250

---
+++ KitayutaMart2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <queue>
+#include <cstring>
+#include <climits>
+#include <cstdio>
+using namespace std;
+typedef pair<int,int> pi;
+typedef set<int> si;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+
+class KitayutaMart2{ 
+        public: 
+        int numBought(int K, int T) { 
+           T /= K;
+           int res = 0;
+           for(int i = 0; i < 30; i++){
+            if((1<<i) & T) res++;
+           }  
+           return res;
+        }
+        
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { int Arg0 = 100; int Arg1 = 100; int Arg2 = 1; verify_case(0, Arg2, numBought(Arg0, Arg1)); }
+	void test_case_1() { int Arg0 = 100; int Arg1 = 300; int Arg2 = 2; verify_case(1, Arg2, numBought(Arg0, Arg1)); }
+	void test_case_2() { int Arg0 = 150; int Arg1 = 1050; int Arg2 = 3; verify_case(2, Arg2, numBought(Arg0, Arg1)); }
+	void test_case_3() { int Arg0 = 160; int Arg1 = 163680; int Arg2 = 10; verify_case(3, Arg2, numBought(Arg0, Arg1)); }
+
+// END CUT HERE
+ 
+}; 
+
+// BEGIN CUT HERE 
+int main(){
+
+        KitayutaMart2 ___test; 
+        ___test.run_test(-1); 
+} 
+// END CUT HERE     
