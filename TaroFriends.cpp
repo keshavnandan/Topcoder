94036272a7ffb43b627b79d78c6c0b8bbbf2026f
keshavnandan <nandan.keshav55@gmail.com>
2015-03-02 20:25:37 +0530

    srm 613 div 1 250

---
+++ TaroFriends.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <queue>
+#include <cstring>
+#include <climits>
+#include <cstdio>
+using namespace std;
+typedef pair<int,int> pi;
+typedef set<int> si;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+typedef long long ll;
+#define sz size()
+#define mp(x, y) make_pair(x, y)
+#define ri(a, b) for(int i=((int)(a)); i < ((int)(b)); i++)                // i -> [a, b)
+#define rie(a, b) for(int i=((int)(a)); i <= ((int)(b)); i++)            // i -> [a, b]
+#define rj(a, b) for(int j=((int)(a)); j < ((int)(b)); j++)               // j -> [a, b)
+#define rje(a, b) for(int j=((int)(a)); j <= ((int)(b)); j++)           // j -> [a, b]
+#define rk(a, b) for(int k=((int)(a)); k < ((int)(b)); k++)           // k -> [a, b)
+#define rke(a, b) for(int k=((int)(a)); k <= ((int)(b)); k++)       // k -> [a, b]
+#define fi(b) for(int i=0; i < ((int)(b)); i++)                             // i -> [0, b)
+#define fie(b) for(int i=0; i <= ((int)(b)); i++)                         // i -> [0, b]
+#define fj(b) for(int j=0; j < ((int)(b)); j++)                            // j -> [0, b)
+#define fje(b) for(int j=0; j <= ((int)(b)); j++)                        // j -> [0, b]
+#define fk(b) for(int k=0; k < ((int)(b)); k++)                        // k -> [0, b)
+#define fke(b) for(int k=0; k < ((int)(b)); k++)                      // k -> [0, b]
+#define fle(b) for(int l=0; l <= ((int)(b)); l++)                        // l -> [0, b]
+
+class TaroFriends{ 
+        public:
+
+        int getNumber(vector <int> coordinates, int X){ 
+            int n = coordinates.size();
+            sort(coordinates.begin(), coordinates.end());
+            int a = coordinates[0], b = coordinates[n-1];
+            int delta = b-a;
+            if(n == 2) return min(delta, abs(delta - 2*X));
+            fi(n-1){
+                int y = max(b-X, coordinates[i]+X), x = min(a+X, coordinates[i+1]-X);
+                delta = min(delta, y-x);
+            }
+            return delta;
+        }
+        
+        
+// BEGIN CUT HERE
+    public:
+    void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); if ((Case == -1) || (Case == 5)) test_case_5(); }
+    private:
+    template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+    void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+    void test_case_0() { int Arr0[] = {-3, 0, 1}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 3; int Arg2 = 3; verify_case(0, Arg2, getNumber(Arg0, Arg1)); }
+    void test_case_1() { int Arr0[] = {4, 7, -7}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 5; int Arg2 = 4; verify_case(1, Arg2, getNumber(Arg0, Arg1)); }
+    void test_case_2() { int Arr0[] = {-100000000, 100000000}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 100000000; int Arg2 = 0; verify_case(2, Arg2, getNumber(Arg0, Arg1)); }
+    void test_case_3() { int Arr0[] = {3, 7, 4, 6, -10, 7, 10, 9, -5}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 7; int Arg2 = 7; verify_case(3, Arg2, getNumber(Arg0, Arg1)); }
+    void test_case_4() { int Arr0[] = {-4, 0, 4, 0}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 4; int Arg2 = 4; verify_case(4, Arg2, getNumber(Arg0, Arg1)); }
+    void test_case_5() { int Arr0[] = {7}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 0; int Arg2 = 0; verify_case(5, Arg2, getNumber(Arg0, Arg1)); }
+
+// END CUT HERE
+ 
+}; 
+
+// BEGIN CUT HERE 
+int main(){
+        
+       TaroFriends ___test; 
+        ___test.run_test(-1); 
+} 
+// END CUT HERE 
