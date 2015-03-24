1e4a12c51712afda7d58df35872ab921d625538e
Keshav Nandan <nandan.keshav55@gmail.com>
2015-03-25 01:28:40 +0530

    srm 558 div 1 250

---
+++ Stamp.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <queue>
+#include <cstring>
+#include <climits>
+#include <cstdio>
+#include <numeric>
+using namespace std;
+typedef pair<int,int> pi;
+typedef set<int> si;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+typedef long long ll;
+#define sz size()
+#define mp make_pair
+#define pb push_back
+#define inf 1000000000
+#define ri(a, b) for(int i=((int)(a)); i < ((int)(b)); i++)                // i -> [a, b)
+#define rie(a, b) for(int i=((int)(a)); i <= ((int)(b)); i++)            // i -> [a, b]
+#define rj(a, b) for(int j=((int)(a)); j < ((int)(b)); j++)               // j -> [a, b)
+#define rje(a, b) for(int j=((int)(a)); j <= ((int)(b)); j++)           // j -> [a, b]
+#define rk(a, b) for(int k=((int)(a)); k < ((int)(b)); k++)           // k -> [a, b)
+#define rke(a, b) for(int k=((int)(a)); k <= ((int)(b)); k++)       // k -> [a, b]
+#define fi(b) for(int i=0; i < ((int)(b)); i++)                             // i -> [0, b)
+#define fie(b) for(int i=0; i <= ((int)(b)); i++)                         // i -> [0, b]
+#define fj(b) for(int j=0; j < ((int)(b)); j++)                            // j -> [0, b)
+#define fje(b) for(int j=0; j <= ((int)(b)); j++)                        // j -> [0, b]
+#define fk(b) for(int k=0; k < ((int)(b)); k++)                        // k -> [0, b)
+#define fke(b) for(int k=0; k < ((int)(b)); k++)                      // k -> [0, b]
+#define fle(b) for(int l=0; l <= ((int)(b)); l++)                        // l -> [0, b]
+int A[55][55];
+string s;
+bool valid(int i, int j){
+    set<char> S;
+    for(int k = i; k <= j; k++)
+        if(s[k] != '*') S.insert(s[k]);
+    return S.size() <= 1;
+}
+
+class Stamp {
+        public:
+        int getMinimumCost(string desiredColor, int stampCost, int pushCost) {
+            s = desiredColor;
+            int mincost = inf, n = desiredColor.size();
+            for(int L = 1; L <= n; L++){
+                fi(55) fj(55) A[i][j] = 0;
+                fie(n-L){
+                    int j = i+L-1;
+                    if(valid(i, j)) A[i][j] = 1;
+                }
+                for(int l = L+1; l <= n; l++)
+                for(int i = 0; i <= n-l; i++){
+                    int j = i+l-1;
+                    if(A[i][j]) continue;
+                    if(valid(i, j)){
+                        A[i][j] = (l+L-1)/L;
+                        continue;
+                    }
+                    int minv = inf;
+                    for(int k = i; k < j; k++){
+                        int a = A[i][k], b = A[k+1][j];
+                        if(a == 0 || b == 0) continue;
+                        minv = min(minv, a+b);
+                    }
+                    if(minv != inf) A[i][j] = minv;
+                }
+                int val =  A[0][n-1];
+                if(val == 0) continue;
+                mincost = min(mincost, stampCost*L + pushCost*val);
+            }
+            return mincost;
+        }
+
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); if ((Case == -1) || (Case == 5)) test_case_5(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { string Arg0 = "RRGGBB"; int Arg1 = 1; int Arg2 = 1; int Arg3 = 5; verify_case(0, Arg3, getMinimumCost(Arg0, Arg1, Arg2)); }
+	void test_case_1() { string Arg0 = "R**GB*"; int Arg1 = 1; int Arg2 = 1; int Arg3 = 5; verify_case(1, Arg3, getMinimumCost(Arg0, Arg1, Arg2)); }
+	void test_case_2() { string Arg0 = "BRRB"; int Arg1 = 2; int Arg2 = 7; int Arg3 = 30; verify_case(2, Arg3, getMinimumCost(Arg0, Arg1, Arg2)); }
+	void test_case_3() { string Arg0 = "R*RR*GG"; int Arg1 = 10; int Arg2 = 58; int Arg3 = 204; verify_case(3, Arg3, getMinimumCost(Arg0, Arg1, Arg2)); }
+	void test_case_4() { string Arg0 = "*B**B**B*BB*G*BBB**B**B*"; int Arg1 = 5; int Arg2 = 2; int Arg3 = 33; verify_case(4, Arg3, getMinimumCost(Arg0, Arg1, Arg2)); }
+	void test_case_5() { string Arg0 = "*R*RG*G*GR*RGG*G*GGR***RR*GG"; int Arg1 = 7; int Arg2 = 1; int Arg3 = 30; verify_case(5, Arg3, getMinimumCost(Arg0, Arg1, Arg2)); }
+
+// END CUT HERE
+
+};
+
+    // BEGIN CUT HERE
+    int main()
+        {
+        Stamp ___test;
+        ___test.run_test(-1);
+        }
+    // END CUT HERE
+
