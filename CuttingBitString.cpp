1f8b2cd49644a9903871f88e27e053df4bc4eb2e
keshavnandan <nandan.keshav55@gmail.com>
2015-04-07 22:59:27 +0530

    srm 555 div 1 250

---
+++ CuttingBitString.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <queue>
+#include <cstring>
+#include <climits>
+#include <cstdio>
+using namespace std;
+typedef pair<int,int> pi;
+typedef set<int> si;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+typedef long long ll;
+#define sz size()
+#define mp make_pair
+#define pb push_back
+#define ri(a, b) for(int i=((int)(a)); i < ((int)(b)); i++)                // i -> [a, b)
+#define rie(a, b) for(int i=((int)(a)); i <= ((int)(b)); i++)            // i -> [a, b]
+#define rj(a, b) for(int j=((int)(a)); j < ((int)(b)); j++)               // j -> [a, b)
+#define rje(a, b) for(int j=((int)(a)); j <= ((int)(b)); j++)           // j -> [a, b]
+#define rk(a, b) for(int k=((int)(a)); k < ((int)(b)); k++)           // k -> [a, b)
+#define rke(a, b) for(int k=((int)(a)); k <= ((int)(b)); k++)       // k -> [a, b]
+#define fi(b) for(int i=0; i < ((int)(b)); i++)                             // i -> [0, b)
+#define fie(b) for(int i=0; i <= ((int)(b)); i++)                         // i -> [0, b]
+#define fj(b) for(int j=0; j < ((int)(b)); j++)                            // j -> [0, b)
+#define fje(b) for(int j=0; j <= ((int)(b)); j++)                        // j -> [0, b]
+#define fk(b) for(int k=0; k < ((int)(b)); k++)                        // k -> [0, b)
+#define fke(b) for(int k=0; k < ((int)(b)); k++)                      // k -> [0, b]
+#define fle(b) for(int l=0; l <= ((int)(b)); l++)                        // l -> [0, b]
+
+class CuttingBitString{
+    bool isPower(ll s){
+        if(s == 1) return true;
+        if(s == 0) return false;
+        return s%5 == 0 && isPower(s/5);
+    }
+    bool isPower(string s){
+        int i = 0;
+        if(s[0] == '0') return false;
+        ll a = stoll(s, NULL, 2);
+        if(a == 1) return true;
+        if(a == 0) return false;
+        return a%5 == 0 && isPower(a/5);
+    }
+
+    public: 
+    int getmin(string s){ 
+
+        int n = s.size();
+
+        int dp[55] = {0};
+        fi(n){
+            string t = s.substr(0, i+1);
+
+            if(isPower(t)){
+                dp[i] = 1;
+                continue;
+            }
+            int minv = 1000;
+            fj(i){
+                if(dp[j] == 0 || !isPower(s.substr(j+1, i-j))) continue;
+                minv = min(minv, dp[j]+1);
+            }
+            if(minv < 1000) dp[i] = minv;
+        }
+        return dp[n-1] == 0 ? -1 : dp[n-1];
+    }
+        
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); if ((Case == -1) || (Case == 5)) test_case_5(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { string Arg0 = "101101101"; int Arg1 = 3; verify_case(0, Arg1, getmin(Arg0)); }
+	void test_case_1() { string Arg0 = "1111101"; int Arg1 = 1; verify_case(1, Arg1, getmin(Arg0)); }
+	void test_case_2() { string Arg0 = "00000"; int Arg1 = -1; verify_case(2, Arg1, getmin(Arg0)); }
+	void test_case_3() { string Arg0 = "110011011"; int Arg1 = 3; verify_case(3, Arg1, getmin(Arg0)); }
+	void test_case_4() { string Arg0 = "1000101011"; int Arg1 = -1; verify_case(4, Arg1, getmin(Arg0)); }
+	void test_case_5() { string Arg0 = "111011100110101100101110111"; int Arg1 = 5; verify_case(5, Arg1, getmin(Arg0)); }
+
+// END CUT HERE
+ 
+        }; 
+
+    // BEGIN CUT HERE 
+    int main()
+        {
+        CuttingBitString ___test; 
+        ___test.run_test(-1); 
+        } 
+    // END CUT HERE 
