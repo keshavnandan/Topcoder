11c2352a45304e988a0cc1035a65d0be3c6a70a2
kenandan <keshav.nandan@myntra.com>
2015-10-12 17:02:33 +0530

    srm 670 div 2 250

---
+++ Cdgame.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <queue>
+#include <cstring>
+#include <climits>
+#include <cstdio>
+#include <numeric>
+using namespace std;
+typedef pair<int,int> pi;
+typedef set<int> si;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+typedef long long ll;
+#define sz size()
+#define mp make_pair
+#define pb push_back
+#define ri(a, b) for(int i=((int)(a)); i < ((int)(b)); i++)                // i -> [a, b)
+#define rie(a, b) for(int i=((int)(a)); i <= ((int)(b)); i++)            // i -> [a, b]
+#define rj(a, b) for(int j=((int)(a)); j < ((int)(b)); j++)               // j -> [a, b)
+#define rje(a, b) for(int j=((int)(a)); j <= ((int)(b)); j++)           // j -> [a, b]
+#define rk(a, b) for(int k=((int)(a)); k < ((int)(b)); k++)           // k -> [a, b)
+#define rke(a, b) for(int k=((int)(a)); k <= ((int)(b)); k++)       // k -> [a, b]
+#define fi(b) for(int i=0; i < ((int)(b)); i++)                             // i -> [0, b)
+#define fie(b) for(int i=0; i <= ((int)(b)); i++)                         // i -> [0, b]
+#define fj(b) for(int j=0; j < ((int)(b)); j++)                            // j -> [0, b)
+#define fje(b) for(int j=0; j <= ((int)(b)); j++)                        // j -> [0, b]
+#define fk(b) for(int k=0; k < ((int)(b)); k++)                        // k -> [0, b)
+#define fke(b) for(int k=0; k < ((int)(b)); k++)                      // k -> [0, b]
+#define fle(b) for(int l=0; l <= ((int)(b)); l++)                        // l -> [0, b]
+
+class Cdgame{
+ 
+        public: 
+        int rescount(vector <int> a, vector <int> b) { 
+        	int s1 = accumulate(a.begin(), a.end(), 0);
+        	int s2 = accumulate(b.begin(), b.end(), 0);
+        	set<int> s;
+            for(int a1 : a)
+            	for(int b1: b){
+            		int k = a1-b1;
+            		s.insert((s1-k)*(s2+k));
+            	}
+
+            return s.size();
+        } 
+        
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); if ((Case == -1) || (Case == 5)) test_case_5(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { int Arr0[] = {1,2}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {3,4}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 2; verify_case(0, Arg2, rescount(Arg0, Arg1)); }
+	void test_case_1() { int Arr0[] = {1,2,4}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {8,16,32}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 9; verify_case(1, Arg2, rescount(Arg0, Arg1)); }
+	void test_case_2() { int Arr0[] = {1,1,1}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {1,1,1}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 1; verify_case(2, Arg2, rescount(Arg0, Arg1)); }
+	void test_case_3() { int Arr0[] = {1,2,3}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {5,5,5}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 3; verify_case(3, Arg2, rescount(Arg0, Arg1)); }
+	void test_case_4() { int Arr0[] = {3,3,4,1}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {2,2,2,100}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 4; verify_case(4, Arg2, rescount(Arg0, Arg1)); }
+	void test_case_5() { int Arr0[] = {31,34,55,56,57}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {1,2,3,4,5}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 15; verify_case(5, Arg2, rescount(Arg0, Arg1)); }
+
+// END CUT HERE
+         
+}; 
+// BEGIN CUT HERE 
+int main(){
+
+        Cdgame ___test; 
+        ___test.run_test(-1); 
+
+}
+// END CUT HERE 
